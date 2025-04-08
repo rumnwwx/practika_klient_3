d9e75b39b7aa12b29675f92f8221b31dd789100b
рума <elchaninapolina@gmail.com>
2025-04-08 13:39:36 +0700

    base code html, js, css

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,1 +1,19 @@
-let app = new Vue()
+let app = new Vue({
+    el: '#app',
+    data: {
+        columns: {
+            planned: [],
+            inProgress: [],
+            testing: [],
+            done: [],
+        },
+        columnTitles: {
+            planned: 'Запланированные задачи',
+            inProgress: 'Задачи в работе',
+            testing: 'Тестирование',
+            done: 'Выполненные задачи',
+        },
+    },
+    methods: {
+    }
+});
